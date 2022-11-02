fcc08ccc7b9f0e96cf69549848d4aa621d88060b
MD-Sohag-Gorami <109500305+MD-Sohag-Gorami@users.noreply.github.com>
2022-11-02 16:04:06 +0600

    Onion Arc

--- Multi lingual student management system/Multi lingual student management system/Controllers/CourseController.cs
+++ Multi lingual student management system/Multi lingual student management system/Controllers/CourseController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Multi_lingual_student_management_system.Factories;
 using Multi_lingual_student_management_system.Services;
 using Multi_lingual_student_management_system.ViewModel;
